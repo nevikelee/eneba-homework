3d9214dcb7528243c53aa8326ec2708e897d4dc6
nevikelee <emilijanikita@gmail.com>
2026-01-03 22:46:02 +0200

    initial setup of the application

---
+++ frontend/search-app/src/App.tsx
@@ -0,0 +1,19 @@
+import Navigation from './components/Navigation.tsx'
+
+function App() {
+  return (
+  <div>
+      <Navigation />
+
+      {/* TODO: GAMES LIST VIEW */}
+
+      <main style={{ paddingTop: '80px' }}>
+         {/* <GameList /> */}
+      </main>
+    
+  </div>
+  );
+  
+}
+
+export default App;
