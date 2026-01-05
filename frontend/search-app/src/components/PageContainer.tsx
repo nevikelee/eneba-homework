453f7a10c49a943caf918c26769df43f6e5ee69b
nevikelee <emilijanikita@gmail.com>
2026-01-05 22:21:17 +0200

    main container to store components

---
+++ frontend/search-app/src/components/PageContainer.tsx
@@ -0,0 +1,16 @@
+
+const PageContainer: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+  <div
+    style={{
+      maxWidth: '1240px',
+      margin: '0 auto',
+      padding: '0 20px',
+      width: '100%',
+      boxSizing: 'border-box',
+    }}
+  >
+    {children}
+  </div>
+);
+
+export default PageContainer;
