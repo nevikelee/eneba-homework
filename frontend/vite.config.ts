34c95f17789b5bcac599f2c0720f619593705b78
nevikelee <emilijanikita@gmail.com>
2026-01-08 18:07:59 +0200

    vite hosts fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // allows external hosts
+    host: '0.0.0.0',
   },
   preview: {
-    host: true, // allows network requests in preview
-    allowedHosts: ['frontend', 'localhost', '127.0.0.1'],
-  },
+    host: '0.0.0.0',
+    port: 80,
+    strictPort: true,
+    allowedHosts: true,
+  }
 });
